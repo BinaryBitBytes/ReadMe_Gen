10d5ddeb341c29c2050e8e8109233880c296679d
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-11 15:40:26 -0500

    updated the readme gen to write the output to a file. Now the return of the confirmation that the file write was successful no longer appears. Also First two objects are undefined, now a problem. Still puts JSON string at header of document.

--- script.js
+++ script.js
@@ -1,59 +1,12 @@
-//installed inquirer using terminal npm i inquirer
-/*
-```md
-AS A developer
-I WANT a README generator
-SO THAT I can quickly create a professional README for a new project
-```
-```md
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for information about my application repository
-THEN a high-quality, professional README.md is generated with the:
-- title of my project 
-- and sections entitled Description,
-- Table of Contents,
-- Installation,
-- Usage,
-- License,
-- Contributing,
-- Tests,
-- and Questions
-WHEN I enter my:
-- project title
-THEN this is displayed as the title of the README
-WHEN I enter a"
-- description, 
-- installation instructions,
-- usage information,
-- contribution guidelines,
-- and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README
-```
-*/
 console.log(process.argv);
 console.log(typeof process.argv);
 //!added node modules: Inquirer and FS
 const inquirer = require("inquirer");
 const fs = require("fs");
-/*fs.readFile('data.csv', 'utf8', (error, data) =>
-    error ? console.error(error) : console.log(data)
-    );
-
-fs.writeFile('log.txt', process.argv[2], (err) =>
-err ? console.error(err) : console.log ('File log complete!')
-);*/
 
 inquirer
   .prompt([
-    Questions={
+    (Questions = {
       type: "input",
       message: "What is your projects name for the readme?",
       name: "nameProject",
@@ -64,7 +17,7 @@ inquirer
           return `An input is needed to continue`;
         }
       },
-    },
+    }),
     {
       type: "input",
       message: "What is your name?",
@@ -167,7 +120,6 @@ inquirer
   ])
 
   .then((data) => {
-    const filename = `${data.nameProject}.md`;
     //const enteredProject = `${data.nameProjectName}`;
     //const enteredUsername = `${data.userName}`;
     //const enteredDescription = `${data.projDescription}`;
@@ -177,84 +129,96 @@ inquirer
     //const enteredTesting = `${data.projTesting}`;
     //const enteredLisence = `${data.projLisence}`;
     //const enteredGitHub = `${data.gHubUser}`;
-    let __TEMPLATE__ = (
-    `#Name of the Project
-    ------------------
-    ${data.nameProjectName}
-
-    ##User Name
-    -----------------
-    ${data.userName}
-
-    ##Description of the project
-    -----------------
-    ${data.projDescription}
-
-    ##Project Installation
-    -----------------
-    ${data.projInstallation}
-
-    ##How to use this Project?
-    -----------------
-    ${data.projUsage}
-
-    ##Project Contributors
-    -----------------
-    ${data.projContributions}
-
-    ##Testing for this Project
-    -----------------
-    ${data.projTesting}
-
-    ##Lisence
-    -----------------
-    ${data.projLisence}
-
-    ##If you have any questions you can reach me at my GitHub.
-    -----------------
-    ${data.gHubUser}
-    `)  
-    fs.writeFile(filename, JSON.stringify(data, null, __TEMPLATE__.data), (err) =>
-      err ? console.log(err) : console.log(
-            `#Name of the Project
-            ------------------
-            ${data.nameProjectName}
-
-            ##User Name
-            -----------------
-            ${data.userName}
-
-            ##Description of the project
-            -----------------
-            ${data.projDescription}
-
-            ##Project Installation
-            -----------------
-            ${data.projInstallation}
-
-            ##How to use this Project?
-            -----------------
-            ${data.projUsage}
-
-            ##Project Contributors
-            -----------------
-            ${data.projContributions}
-
-            ##Testing for this Project
-            -----------------
-            ${data.projTesting}
-
-            ##Lisence
-            -----------------
-            ${data.projLisence}
-
-            ##If you have any questions you can reach me at my GitHub.
-            -----------------
-            ${data.gHubUser}
-            `)
+    //!TEMPLATE VARIABLES
+    //! Writing Filename
+    const filename = `${data.nameProject}.md`;
+    fs.writeFile(filename, JSON.stringify(data, null), (err) =>
+      err ? console.log(err) : console.log(`logged file`)
+    );
+    //! Reading Filename for writing data
+    fs.readFile(filename, "utf8", (error, data) =>
+      error ? console.error(error) : console.log(data)
+    );
+    //! TEMPLATE
+    const __TEMPLATE__ = `#Name of the Project\n
+  ------------------\n
+  ${data.nameProjectName}\n
+  \n
+  ##User Name\n
+  -----------------\n
+  ${data.userName}\n
+  \n
+  ##Description of the project\n
+  -----------------\n
+  ${data.projDescription}\n
+  \n
+  ##Project Installation\n
+  -----------------\n
+  ${data.projInstallation}\n
+  \n
+  ##How to use this Project?\n
+  -----------------\n
+  ${data.projUsage}\n
+  \n
+  ##Project Contributors\n
+  -----------------\n
+  ${data.projContributions}\n
+  \n
+  ##Testing for this Project\n
+  -----------------\n
+  ${data.projTesting}\n
+  \n
+  ##Lisence\n
+  -----------------\n
+  ${data.projLisence}\n
+  \n
+  ##If you have any questions you can reach me at my GitHub.\n
+  -----------------\n
+  ${data.gHubUser}\n
+  `;
+    fs.appendFile(filename, `\n ${__TEMPLATE__}`, (err) =>
+      err
+        ? console.log(err)
+        : console.log(
+            `#Name of the Project \n
+            ------------------ \n
+            ${data.nameProjectName}\n
+            \n
+            ##User Name\n
+            -----------------\n
+            ${data.userName}\n
+            \n
+            ##Description of the project\n
+            -----------------\n
+            ${data.projDescription}\n
+            \n
+            ##Project Installation\n
+            -----------------\n
+            ${data.projInstallation}\n
+            \n
+            ##How to use this Project?\n
+            -----------------\n
+            ${data.projUsage}\n
+            \n
+            ##Project Contributors\n
+            -----------------\n
+            ${data.projContributions}\n
+            \n
+            ##Testing for this Project\n
+            -----------------\n
+            ${data.projTesting}\n
+            \n
+            ##Lisence\n
+            -----------------\n
+            ${data.projLisence}\n
+            \n
+            ##If you have any questions you can reach me at my GitHub.\n
+            -----------------\n
+            ${data.gHubUser}\n
+            `
+          )
     );
   });
-
 /*
         if ((data) => 
         data.nameUser === data.input
