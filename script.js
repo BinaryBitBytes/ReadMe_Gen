53ff9f603b26f47073b7ca25763f63ff17d6e70c
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-13 19:58:52 -0400

    constructed the file markDown.js to generate the html template, added the require markdown.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log(process.argv);
 console.log(typeof process.argv);
 //!added node modules: Inquirer and FS
+const generateMarkdown = require("./utils/generateMarkdown.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
@@ -97,8 +98,7 @@ inquirer
       type: "list",
       message: ["What Lisences would you like to use?"],//, <a src=`script.js.${projLisence}`>],
       name: "projLisence",
-      choices: ["none", "Apache License 2.0", "GNU General Public License v3.0", 'MIT License', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 
-      'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1','The Unlicense'],
+      choices: ['MIT', 'GPL-3.0', 'Apache-2.0','NONE'],
       validate: (blankInput) => {
         if (blankInput) {
           return true;
@@ -119,9 +119,75 @@ inquirer
         }
       },
     },
+    {
+      type: "input",
+      message:
+        "If someone needs to reach you please provide your email for contact?",
+      name: "projEmail",
+      validate: (blankInput) => {
+        if (blankInput) {
+          return true;
+        } else {
+          return `An input is needed to continue`;
+        }
+      },
+    },
   ])
-
+  
   .then((data) => {
+    const filename = `${data.nameProject}.md`;
+      fs.writeFile(filename, JSON.stringify(`The Official Readme for ${data.nameProject}`, null), (err) =>
+        err ? console.log(err) : console.log(`logged file`)
+      );
+      //! Reading Filename for writing data
+      fs.readFile(filename, "utf8", (error, data) =>
+        error ? console.error(error) : console.log(data)
+      );
+      fs.appendFile(filename, `\n ${generateMarkdown(data)}`, (err) =>
+        err
+          ? console.log(err)
+          : console.log(
+              `#Name of the Project \n
+              ------------------ \n
+              ${data.nameProjectName}\n
+              ${console.log(typeof data.nameProjectName )}
+              \n
+              ##User Name\n
+              -----------------\n
+              ${data.userName}\n
+              ${console.log(typeof data.userName )}
+              \n
+              ##Description of the project\n
+              -----------------\n
+              ${data.projDescription}\n
+              \n
+              ##Project Installation\n
+              -----------------\n
+              ${data.projInstallation}\n
+              \n
+              ##How to use this Project?\n
+              -----------------\n
+              ${data.projUsage}\n
+              \n
+              ##Project Contributors\n
+              -----------------\n
+              ${data.projContributions}\n
+              \n
+              ##Testing for this Project\n
+              -----------------\n
+              ${data.projTesting}\n
+              \n
+              ##Lisence\n
+              -----------------\n
+              ${data.projLisence}\n
+              \n
+              ##If you have any questions you can reach me at my GitHub.\n
+              -----------------\n
+              ${data.gHubUser}\n
+              `
+            )
+      );
+    });
     //const enteredProject = `${data.nameProjectName}`;
     //const enteredUsername = `${data.userName}`;
     //const enteredDescription = `${data.projDescription}`;
@@ -142,96 +208,45 @@ inquirer
     ////  error ? console.error(error) : console.log(data)
     /////);
     //! TEMPLATE
+    /*
     const __TEMPLATE__ = `\n
   ##Name of the Project\n
-  ------------------\n
+  ============\n
   ${data.nameProject}\n
   \n
   ##User Name\n
-  -----------------\n
+  ============\n
   ${data.nameUser}\n
   \n
   ##Description of the project\n
-  -----------------\n
+  ============\n
   ${data.projDescription}\n
   \n
   ##Project Installation\n
-  -----------------\n
+  ============\n
   ${data.projInstallation}\n
   \n
   ##How to use this Project?\n
-  -----------------\n
+  ============\n
   ${data.projUsage}\n
   \n
   ##Project Contributors\n
-  -----------------\n
+  ============\n
   ${data.projContributions}\n
   \n
   ##Testing for this Project\n
-  -----------------\n
+  ============\n
   ${data.projTesting}\n
   \n
   ##Lisence\n
-  -----------------\n
+  ============\n
   ${data.projLisence}\n
   \n
   ##If you have any questions you can reach me at my GitHub.\n
-  -----------------\n
+  ============\n
   ${data.gHubUser}\n
   `;
-  const filename = `${data.nameProject}.md`;
-    fs.writeFile(filename, JSON.stringify(`The Official Readme for ${data.nameProject}`, null), (err) =>
-      err ? console.log(err) : console.log(`logged file`)
-    );
-    //! Reading Filename for writing data
-    fs.readFile(filename, "utf8", (error, data) =>
-      error ? console.error(error) : console.log(data)
-    );
-    fs.appendFile(filename, `\n ${__TEMPLATE__}`, (err) =>
-      err
-        ? console.log(err)
-        : console.log(
-            `#Name of the Project \n
-            ------------------ \n
-            ${data.nameProjectName}\n
-            ${console.log(typeof data.nameProjectName )}
-            \n
-            ##User Name\n
-            -----------------\n
-            ${data.userName}\n
-            ${console.log(typeof data.userName )}
-            \n
-            ##Description of the project\n
-            -----------------\n
-            ${data.projDescription}\n
-            \n
-            ##Project Installation\n
-            -----------------\n
-            ${data.projInstallation}\n
-            \n
-            ##How to use this Project?\n
-            -----------------\n
-            ${data.projUsage}\n
-            \n
-            ##Project Contributors\n
-            -----------------\n
-            ${data.projContributions}\n
-            \n
-            ##Testing for this Project\n
-            -----------------\n
-            ${data.projTesting}\n
-            \n
-            ##Lisence\n
-            -----------------\n
-            ${data.projLisence}\n
-            \n
-            ##If you have any questions you can reach me at my GitHub.\n
-            -----------------\n
-            ${data.gHubUser}\n
-            `
-          )
-    );
-  });
+  */
 /*
         if ((data) => 
         data.nameUser === data.input
---
+++ utils/markDown.js
@@ -0,0 +1,74 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    switch (license) {
+      case "MIT":
+        return `https://img.shields.io/badge/license-MIT-blue`;
+        break;
+      case "GPL-3.0":
+        return `https://img.shields.io/badge/license-GPL--3.0-blue`;
+        break;
+      case "Apache-2.0":
+        return `https://img.shields.io/badge/license-Apache--2.0-blue`;
+        break;
+      default:
+        return "";
+        break;
+    }
+  }
+  
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    switch (license) {
+      case "MIT":
+        return `https://choosealicense.com/licenses/mit/`;
+        break;
+      case "GPL-3.0":
+        return `https://www.gnu.org/licenses/gpl-3.0.en.html`;
+        break;
+      case "Apache-2.0":
+        return `http://www.apache.org/licenses/LICENSE-2.0`;
+        break;
+      default:
+        return "";
+        break;
+    }
+  }
+  
+  //! Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    return `![${license}](${renderLicenseBadge(license)})
+    ### [${renderLicenseLink(license)}](${renderLicenseLink(license)})
+    `;
+  }
+  
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.nameProjectName}
+    ## Description
+    ${data.projDescription}
+    ## Table of Contents:
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Testing](#tests)
+    - [Questions](#questions)
+    ## Installation
+    ${data.projInstallation}
+    ## Usage
+    ${data.projUsage}
+    ## License
+    ### ${renderLicenseSection(data.projLisence)}
+    ## Contributions
+    ${data.projContributions}
+    ## Testing
+    ${data.projTesting}
+    
+    ## Questions
+    [Github Profile](https://github.com/${data.gHubUser}) | To reach me via Email ${data.projEmail}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
